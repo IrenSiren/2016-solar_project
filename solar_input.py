5fad68a84cd016a06d24d295c8fd5abc2a1cd078
MechHedgehog <71868148+MechHedgehog@users.noreply.github.com>
2020-11-06 11:17:01 +0300

    Update solar_input.py

--- solar_input.py
+++ solar_input.py
@@ -82,10 +82,8 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
-            print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
-            # FIXME: should store real values
-
-# FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
+            print(out_file, "%s %d %s %f %d %d %d %d" % (obj.type, obj.R, obj.color, obj.m, obj.x, obj.y, obj.Vx, obj.Vy))
+           
 
 if __name__ == "__main__":
     print("This module is not for direct call!")
